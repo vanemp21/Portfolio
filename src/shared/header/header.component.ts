37f9a8e8cbdc3630826d3109ae815fc51b34db40
Vanessa <vanemp21@gmail.com>
2024-04-23 11:40:01 +0200

    feat(add): Added hamburguer for small resolution

--- src/shared/header/header.component.ts
+++ src/shared/header/header.component.ts
@@ -1,13 +1,22 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isOpen = false;
+  isFixedMenuVisible = false;
 
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+    this.isFixedMenuVisible = this.isOpen;
+
+
+  }
 }
