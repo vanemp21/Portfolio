70dbae33d771ff0caae3b9fa73e55110dc12755e
Vanessa <vanemp21@gmail.com>
2024-04-24 22:07:25 +0200

    feat(change): Changed personal experience section

--- src/app/body/experience/experience.component.ts
+++ src/app/body/experience/experience.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import proyectosData from '../experience/experience.json';
+import idiomasData from '../experience/languages.json';
 
 @Component({
   selector: 'app-experience',
@@ -11,100 +13,8 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 })
 export class ExperienceComponent {
   mostrar: boolean = false;
-  proyectos = [
-
-    {
-      id: 'proyectoSpotify',
-      nombre: 'Proyecto Spotify',
-      tecnologia: 'Angular',
-      descripcionKey: 'proyectoSpotifyDescripcion',
-      githubUrl: 'https://github.com/vanemp21/Proyecto-Spotify--Angular',
-      etiquetas: ['Angular', 'TypeScript', 'Bootstrap', 'CSS', 'HTML'],
-      mostrado: false,
-    },
-    {
-      id: 'proyectoTienda',
-      nombre: 'Proyecto Tienda',
-      tecnologia: 'Angular',
-      descripcionKey: 'proyectoTiendaDescripcion',
-      githubUrl: 'https://github.com/vanemp21/Proyecto-Tienda--Angular',
-      etiquetas: ['Angular', 'TypeScript', 'Bootstrap', 'CSS', 'HTML', 'Node','Express','MySQL'],
-      mostrado: false,
-    },
-    {
-      id: 'proyectoCalculadora',
-      nombre: 'Proyecto Calculadora',
-      tecnologia: 'Angular',
-      descripcionKey: 'proyectoCalculadoraDescripcion',
-      githubUrl: 'https://github.com/vanemp21/Proyecto-Calculadora--Angular',
-      etiquetas: ['Angular', 'TypeScript', 'Bootstrap', 'CSS', 'HTML'],
-      mostrado: false,
-    },
-    {
-      id: 'proyectoHeroes',
-      nombre: 'Proyecto Héroes',
-      tecnologia: 'ReactJs',
-      descripcionKey: 'proyectoHeroesDescripcion',
-      githubUrl: 'https://github.com/vanemp21/Proyecto-heroes--ReactJs',
-      etiquetas: [
-        'ReactJs',
-        'CSS',
-        'HTML',
-        'Firebase',
-        'Tailwind',
-        'JavaScript',
-      ],
-      mostrado: false,
-    },
-    {
-      id: 'proyectoCalendario',
-      nombre: 'Proyecto Calendario',
-      tecnologia: 'ReactJs',
-      descripcionKey: 'proyectoCalendarioDescripcion',
-      githubUrl: 'https://github.com/vanemp21/Proyecto-Calendario--ReacJS',
-      etiquetas: [
-        'ReactJs',
-        'CSS',
-        'HTML',
-        'Tailwind',
-        'JavaScript',
-        'Firebase',
-      ],
-      mostrado: false,
-    },
-    {
-      id: 'proyectoAgenda',
-      nombre: 'Proyecto Agenda',
-      tecnologia: 'ReactJs',
-      descripcionKey: 'proyectoAgendaDescripcion',
-      githubUrl: 'https://github.com/vanemp21/Proyecto-journal--ReactJs',
-      etiquetas: [
-        'ReactJs',
-        'CSS',
-        'HTML',
-        'Tailwind',
-        'JavaScript',
-        'Firebase',
-      ],
-      mostrado: false,
-    },
-    {
-      id: 'proyectoTfg',
-      nombre: 'Proyecto TFG',
-      tecnologia: 'PHP',
-      descripcionKey: 'proyectoTfgDescripcion',
-      githubUrl: 'https://github.com/vanemp21/Proyecto-TFG--PHP',
-      etiquetas: ['PHP', 'JavaScript', 'Bootstrap', 'CSS', 'HTML', 'MySQL'],
-      mostrado: false,
-    },
-  ];
-  idiomas = [
-    { nombre: 'Español', puntuacion: 5 },
-    { nombre: 'Inglés', puntuacion: 4 },
-    { nombre: 'Valenciano', puntuacion: 5 },
-    { nombre: 'Catalán', puntuacion: 5 },
-    { nombre: 'Alemán', puntuacion: 2 },
-  ];
+  proyectos = proyectosData;
+  idiomas = idiomasData;
 
   generarEstrellas(puntuacion: number): any[] {
     return Array(puntuacion)
