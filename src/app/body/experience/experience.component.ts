5a2028a9b8cc7a85c4c693a21b803df058f951cc
Vanessa <vanemp21@gmail.com>
2024-06-16 20:10:20 +0200

    feat(fix): fixed some CSS and changed languages style

--- src/app/body/experience/experience.component.ts
+++ src/app/body/experience/experience.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule} from '@angular/common';
 import { Component } from '@angular/core';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 import projectsData from '../experience/experience.json';
-import languagesData from '../experience/languages.json';
+
 
 @Component({
   selector: 'app-experience',
@@ -14,13 +14,9 @@ import languagesData from '../experience/languages.json';
 export class ExperienceComponent {
   mostrar: boolean = false;
   proyectos = projectsData;
-  idiomas = languagesData;
 
-  generarEstrellas(puntuacion: number): any[] {
-    return Array(puntuacion)
-      .fill(0)
-      .map((x, i) => i);
-  }
+
+   
   mostrarTexto(proyecto: any) {
     proyecto.mostrado = !proyecto.mostrado;
   }
