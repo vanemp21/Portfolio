6049612fef289521cc8dee87be9f434540ed0b19
Vanessa <vanemp21@gmail.com>
2024-06-19 15:23:43 +0200

    feat(change): Changed projects section

--- src/app/body/experience/experience.component.ts
+++ src/app/body/experience/experience.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule} from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import projectsData from '../experience/experience.json';
+ 
 
 
 @Component({
@@ -12,14 +12,5 @@ import projectsData from '../experience/experience.json';
   styleUrl: './experience.component.css',
 })
 export class ExperienceComponent {
-  mostrar: boolean = false;
-  proyectos = projectsData;
-
-
-   
-  mostrarTexto(proyecto: any) {
-    proyecto.mostrado = !proyecto.mostrado;
-  }
-
-
+ 
 }
